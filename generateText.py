d0d308f4ea8f41d7bf250e237b169029c3ff5592
Mark Apinis <mapinis2019@gmail.com>
2019-05-22 21:12:36 -0400

    generateText.py

--- generateText.py
+++ generateText.py
@@ -1,6 +1,17 @@
 from textgenrnn import textgenrnn
+import os
 
 generator = textgenrnn()
-generator.train_from_largetext_file('data/text/502.txt', new_model=True);
+
+files = os.listdir('data/text')
+
+print("Training on", files[0])
+generator.train_from_largetext_file('data/text/' + files[0], new_model=True, num_epochs=2, batch_size=256, validate=False, dropout=20)
+
+for fileName in files[1:]:
+    print("Training on", fileName)
+    generator.train_from_largetext_file('data/text/' + fileName, new_model=False, num_epochs=2, batch_size=256, validate=False, dropout=20)
+
+generator.save('2Epochs.hdf5')
 
 print(generator.generate_samples())
