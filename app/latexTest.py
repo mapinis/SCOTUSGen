de60fefa27d214843e6d03eb9db0f456bc5188ba
Mark Apinis <mapinis2019@gmail.com>
2019-05-28 23:24:32 -0400

    Working test tex substitution

--- app/latexTest.py
+++ app/latexTest.py
@@ -5,7 +5,18 @@ from latex import build_pdf
 env = make_env(loader=FileSystemLoader("./templates/"))
 template = env.get_template("opinion.tex")
 
-pdf = build_pdf(template.render(text="test"))
+pdf = build_pdf(
+    template.render(
+        petitioner="APPLE INC.",
+        respondent="ROBERT PEPPER",
+        date="May 29, 2019",
+        justice="Ginsburg",
+        number1="27",
+        number2="4664",
+        circuit="NINTH",
+        text="test",
+    )
+)
 
 with open("opinions/test.pdf", "wb") as f:
     f.write(bytes(pdf))
